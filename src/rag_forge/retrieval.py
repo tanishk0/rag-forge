b95a04fdd27c9148050065b5a5af36ce6ecdbcc2
tanishk0 <tanishkaggarwal245@gmail.com>
2026-09-26 16:27:50 +0530

    added basic retrieval with top k chunks

---
+++ src/rag_forge/retrieval.py
@@ -0,0 +1,16 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from .models import Chunk
+from .database import engine
+from .embedding import create_embedding
+
+def retrieve(query: str, top_k: int =5):
+    query_embedding = create_embedding(query)
+
+    with Session(engine) as session:
+        results = session.execute(
+            select(Chunk).order_by(Chunk.embedding.cosine_distance(query_embedding).limit(top_k))
+        ).scalars().all()
+
+        return results
